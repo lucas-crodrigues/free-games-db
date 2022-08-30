41b972ecca5f42ca5473e1452835b1860506f390
lucascr <lcrodrigues1995@gmail.com>
2022-08-30 11:45:02 -0300

    add url paths

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import './App.css';
+import React from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { getGames } from "./redux/games/fetchGames";
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/home';
-import Games from './components/games';
+import Home from './pages/home';
+import Games from './pages/details';
+import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const games = useSelector((state) => state.games);
+
+  React.useEffect(() => {
+    if (!games.length) {
+      dispatch(getGames());
+    }
+  });
+
+  const filterList = (data, query) => {
+    return data.filter(name => name.platforms.toString().toUpperCase().includes(query.toUpperCase()));
+  };
+
+  const pcGames = filterList(games, 'PC');
+  const consoleGames = filterList(games, 'xbox' || 'playstation' || 'nintendo');
+  const mobileGames = filterList(games, 'android' || 'ios');
+
+  const DEBUG = () => {
+    console.log(mobileGames);
+  };
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/games" element={<Games />} />
+        <Route path="/" element={<Home key={"games"} games={games} pcGames={pcGames} consoleGames={consoleGames} mobileGames={mobileGames} />} />
+        <Route path="/games" element={<Games key={"gamesPage"} props={games} />} />
+        <Route path="/pc" element={<Games key={"gamesPage"} props={pcGames} />} />
+        <Route path="/console" element={<Games key={"gamesPage"} props={consoleGames} />} />
+        <Route path="/mobile" element={<Games key={"gamesPage"} props={mobileGames} />} />
       </Routes>
+      <button type="button" onClick={DEBUG}>debug app</button>
     </div>
   );
 }
