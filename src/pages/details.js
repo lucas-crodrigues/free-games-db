f1151449b621063eab855fd69d3dc172c935cf94
lucascr <lcrodrigues1995@gmail.com>
2022-08-30 17:59:26 -0300

    Populate pages

--- src/pages/details.js
+++ src/pages/details.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Game from "../components/Game";
+import "./details.css";
 
-const Games = () => (
-  <>
-    <div className="header">
-      <NavLink to="/" >‹</NavLink>
-      <h1>Free Games DB</h1>
-    </div>
-    <div>
-      games
-    </div>
-  </>
-)
+const Games = (props) => {
+  const { gameList, icon, title } = props;
+  
+  return (
+    <>
+      <div className="header">
+        <NavLink to="/" >‹</NavLink>
+        <h1>Free Games DB</h1>
+      </div>
+      <div className="pageTitle">
+        <img src={icon} />
+        <h2>{title}</h2>
+      </div>
+      <div className="gamesList">
+        {gameList.map(game => <Game key={game.id} game={game} />)}
+      </div>
+    </>
+  );
+};
 
 export default Games;
