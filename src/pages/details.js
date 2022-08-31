1312f1192a7e224d727d2aa2505c2bebc7271b1a
lucascr <lcrodrigues1995@gmail.com>
2022-08-31 14:46:33 -0300

    Add tabs on the details page

--- src/pages/details.js
+++ src/pages/details.js
@@ -5,7 +5,7 @@ import "./details.css";
 
 const Games = (props) => {
   const { gameList, icon, title } = props;
-  
+
   return (
     <>
       <div className="header">
@@ -13,8 +13,14 @@ const Games = (props) => {
         <h1>Free Games DB</h1>
       </div>
       <div className="pageTitle">
-        <img src={icon} />
+        <img src={icon} alt="logo"/>
         <h2>{title}</h2>
+      </div>
+      <div className="filters">
+        <NavLink to="/games">All Free Content</NavLink>
+        <NavLink to="/pc">All PC Content</NavLink>
+        <NavLink to="/console">All Console Content</NavLink>
+        <NavLink to="/mobile">All Mobile Content</NavLink>
       </div>
       <div className="gamesList">
         {gameList.map(game => <Game key={game.id} game={game} />)}
