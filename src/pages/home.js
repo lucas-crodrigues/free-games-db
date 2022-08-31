c33850e0b183a97826ae4be73e15333d526d53d4
lucascr <lcrodrigues1995@gmail.com>
2022-08-31 14:47:50 -0300

    Add logo and stats section

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,41 +1,73 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import gameIcon from '../art/game_icon.svg'
-import pcIcon from "../art/pc_icon.svg";
-import consoleIcon from "../art/console_icon.svg";
-import mobileIcon from "../art/mobile_icon.svg";
+import logo from "../art/logo.svg";
 import "./home.css";
 
 const Home = (props) => {
-  const { games, pcGames, consoleGames, mobileGames } = props;
+  const { games, pcGames, consoleGames, mobileGames, allGames, allDLC, allEarly } = props;
 
   return (
     <>
       <div className="homePage">
         <div className="header">
-          <NavLink to="/" >‹</NavLink>
+          <div className="invisible"></div>
           <h1>Free Games DB</h1>
         </div>
-        <div className="stats">
-          <NavLink to="/games">
-            <img src={gameIcon} />
-            All Free Content: {games.length}
-          </NavLink>
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="main">
+          <h3>STATS</h3>
+          <div className="stats">
+            <div>
+              <small>All<br/>games</small>
+              <p>{games.length}</p>
+            </div>
+            <div>
+              <small>PC<br/>games</small>
+              <p>{pcGames.length}</p>
+            </div>
+            <div>
+              <small>Console<br/>games</small>
+              <p>{consoleGames.length}</p>
+            </div>
+            <div>
+              <small>Mobile<br/>games</small>
+              <p>{mobileGames.length}</p>
+            </div>
+            <div>
+              <small>Full<br/>games</small>
+              <p>{allGames.length}</p>
+            </div>
+            <div>
+              <small>All<br/>DLCs</small>
+              <p>{allDLC.length}</p>
+            </div>
+            <div>
+              <small>Early<br/>Access</small>
+              <p>{allEarly.length}</p>
+            </div>
+          </div>
         </div>
         <div className="pages">
+          <div className="gameContent page">
+            <NavLink to="/games" className="pageLink">
+              All Free Content: {games.length}
+            </NavLink>
+          </div>
           <div className="pcContent page">
             <NavLink to="/pc" className="pageLink">
-              <img src={pcIcon} className="pagesIcon" />All PC Content: {pcGames.length}
+              All PC Content: {pcGames.length}
             </NavLink>
           </div>
           <div className="consoleContent page">
             <NavLink to="/console" className="pageLink">
-              <img src={consoleIcon} className="pagesIcon" />All Console Content: {consoleGames.length}
+              All Console Content: {consoleGames.length}
             </NavLink>
           </div>
           <div className="mobileContent page">
             <NavLink to="/mobile" className="pageLink">
-              <img src={mobileIcon} className="pagesIcon" />All Mobile Content: {mobileGames.length}
+              All Mobile Content: {mobileGames.length}
             </NavLink>
           </div>
         </div>
