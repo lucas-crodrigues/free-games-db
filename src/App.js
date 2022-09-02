52430af465c2668399d8343293346d5abf4b35de
lucascr <lcrodrigues1995@gmail.com>
2022-09-02 16:58:00 -0300

    fix linters

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import {
   BrowserRouter as Router, Routes, Route,
 } from 'react-router-dom';
-import Home from './pages/Home';
-import Details from './pages/Details';
+import Home from './pages/Home.js';
+import Details from './pages/Details.js';
 import './App.css';
 
 function App() {
